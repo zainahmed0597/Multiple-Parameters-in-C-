b92afeae2705d8f8d3bb2723ddc994f891cb1a4a
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2020-02-09 19:46:36 +0500

    First commit

---
+++ Multiple Parameters.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int addNums(int x, int y){
+	int result = x + y;
+	return result;
+}
+
+int main(){
+	
+	int x = addNums(10, 20);
+	
+	cout << x <<  endl;
+	
+}
